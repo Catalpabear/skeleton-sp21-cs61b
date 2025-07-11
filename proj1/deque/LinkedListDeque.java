36887cef177a32bbdc8063790230997b360756fd
catabear <3447712782@qq.com>
2025-07-11 11:05:22 +0800

    lldeque have a bug

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,128 @@
+package deque;
+
+public class LinkedListDeque<T>  {
+
+    private class Node<T> {
+        private T data;
+        private Node<T>prev;
+        private Node<T> next;
+        public Node(T data, Node<T> prev, Node<T> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private int size;
+    private Node<T>sentinel;
+    private Node<T>curr_head,curr_tail;
+
+    public LinkedListDeque() {
+        sentinel = new Node<T>(null, null, null);
+        curr_head = sentinel;
+        curr_tail = sentinel;
+        size=0;
+    }
+    public LinkedListDeque(T head_data) {
+        sentinel=new Node<>(null, null, null);
+        curr_head= new Node<>(head_data, null, sentinel);
+        sentinel.prev=curr_head;
+        curr_tail=sentinel;
+        size=1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addFirst(T data) {
+        curr_head.prev=new Node<>(data, null, curr_head);
+        curr_head=curr_head.prev;
+        size++;
+    }
+    public void addLast(T data) {
+        curr_tail.next=new Node<>(data, curr_tail, null);
+        curr_tail=curr_tail.next;
+        size++;
+    }
+
+    public T removeFirst(){
+        if(curr_head==sentinel){
+            if(curr_head.next==null){
+                return null;
+            }else{
+                T data=curr_head.next.data;
+                curr_head=curr_head.next.next;
+                size--;
+                return data;
+            }
+        }else{
+            if(curr_head==null){
+                return null;
+            }else {
+                T data=curr_head.data;
+                curr_head=curr_head.next;
+                size--;
+                return data;
+            }
+        }
+    }
+    public T removeLast(){
+        if(curr_tail==sentinel){
+            if(curr_tail.prev==null){
+                return null;
+            }else{
+                T data=curr_tail.prev.data;
+                curr_tail=curr_tail.prev.prev;
+                size--;
+                return data;
+            }
+        }else{
+            if(curr_tail==null){
+                return null;
+            }else{
+                T data=curr_tail.data;
+                curr_tail=curr_tail.prev;
+                size--;
+                return data;
+            }
+        }
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    public void printDeque(){
+        Node<T> move=curr_head;
+        while(move!=null){
+            if(move==sentinel){
+                move=move.next;
+                continue;
+            }
+            System.out.print(move.data+" ");
+            move=move.next;
+        }
+        System.out.println();
+    }
+
+    public T get(int x){
+        Node<T> move=curr_head;
+        while(x>0){
+            move=move.next;
+            x--;
+        }
+        return move.data;
+    }
+    public T getRecursive(int index){
+       return Recursive_helper(index,curr_head);
+    }
+    private T Recursive_helper(int index,Node<T> move){
+       if(index==0) {
+           return move.data;
+       }else{
+           return Recursive_helper(index-1,move.next);
+       }
+    }
+
+}
